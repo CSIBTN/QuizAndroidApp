944bd56ae15bc2a48154165c09a3c2a8eef51de9
CSIBTN <krutinivan3@gmail.com>
2022-05-30 19:49:39 +0300

    Added Hightlighting

--- app/src/main/java/com/csibtn/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/csibtn/quizapp/QuizQuestionsActivity.kt
@@ -1,26 +1,34 @@
 package com.csibtn.quizapp
+import android.graphics.Color
+import android.graphics.Typeface
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.csibtn.quizapp.databinding.ActivityQuizQuestionsBinding
 
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
 
-
+    private var currentPosition : Int = 1
+    private lateinit var questions : MutableList<Question>
+    private var selectedOptionPosition = 0
     private lateinit var tvProgress : TextView
     private lateinit var progressBar : ProgressBar
     private lateinit var  imageView : ImageView
     private lateinit var  tvQuestion: TextView
 
-
     private lateinit var firstOption : TextView
     private lateinit var secondOption : TextView
     private lateinit var thirdOption : TextView
     private lateinit var fourthOption : TextView
+
+    private lateinit var btnSubmit : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,8 +43,20 @@ class QuizQuestionsActivity : AppCompatActivity() {
         secondOption = bindingQuiz.secondOption
         thirdOption = bindingQuiz.thirdOption
         fourthOption = bindingQuiz.fourthOption
+        btnSubmit = bindingQuiz.btnSubmit
 
-        val questions = Constants.getQuestions()
+        firstOption.setOnClickListener(this)
+        secondOption.setOnClickListener(this)
+        thirdOption.setOnClickListener(this)
+        fourthOption.setOnClickListener(this)
+
+        btnSubmit.setOnClickListener(this)
+        questions = Constants.getQuestions()
+
+        setQuestion()
+    }
+
+    private fun setQuestion() {
 
         var currentPosition = 1
         val question = questions[currentPosition - 1]
@@ -48,5 +68,42 @@ class QuizQuestionsActivity : AppCompatActivity() {
         secondOption.text = question.secondOption
         thirdOption.text = question.thirdOption
         fourthOption.text = question.fourthOption
+
+        if(currentPosition == questions.size){
+            btnSubmit.text = "finish"
+        }
+    }
+
+    private fun defaultOptionsView(){
+        val options = mutableListOf<TextView>()
+        options.add(firstOption)
+        options.add(secondOption)
+        options.add(thirdOption)
+        options.add(fourthOption)
+
+        for(option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this,R.drawable.option_background)
+        }
+    }
+    private fun selectedOptionView(tv : TextView, selectedOptNum: Int){
+        defaultOptionsView()
+        selectedOptionPosition = selectedOptNum
+        tv.setTextColor(Color.parseColor("#363A43"))
+        tv.setTypeface(tv.typeface,Typeface.BOLD)
+        tv.background = ContextCompat.getDrawable(this,R.drawable.selected_option_borderr_bg)
+    }
+
+
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            firstOption.id -> selectedOptionView(firstOption,1)
+            secondOption.id -> selectedOptionView(secondOption,2)
+            thirdOption.id -> selectedOptionView(thirdOption,3)
+            fourthOption.id -> selectedOptionView(fourthOption,4)
+            btnSubmit.id -> {}//TODO
+        }
     }
 }
