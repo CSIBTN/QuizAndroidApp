88ec81267bfe5b3db5116daf31006ba74a952161
CSIBTN <krutinivan3@gmail.com>
2022-05-30 16:32:46 +0300

    Added questions

---
+++ app/src/main/java/com/csibtn/quizapp/Constants.kt
@@ -0,0 +1,98 @@
+package com.csibtn.quizapp
+
+object Constants {
+
+    fun getQuestions():MutableList<Question>{
+        val questionsList = mutableListOf<Question>()
+        val question1 = Question(
+            1,
+            "Which country is represented by this flag?",
+             R.drawable.flag_of_north_macedonia,
+             "Macedonia",
+            "Great Britain",
+            "Australia",
+            "Ukraine",
+            1
+        )
+        val question2 = Question(
+            1,
+            "Which continent is the driest?",
+            R.drawable.driest,
+            "Antarctica",
+            "Australia",
+            "Eurasia",
+            "South America",
+            1
+        )
+        val question3 = Question(
+            1,
+            "What is the most deadly animal on Earth?",
+            R.drawable.deadly,
+            "White Shark",
+            "Black Mamba",
+            "Golden Tree Frog",
+            "Black Scorpion",
+            3
+        )
+        val question4 = Question(
+            1,
+            "What is the fastest animal on Earth?",
+            R.drawable.fastest,
+            "Lion",
+            "Horse",
+            "Bold Eagle",
+            "Cheetah",
+            4
+        )
+        val question5 = Question(
+            1,
+            "What is the biggest animal in the world?",
+            R.drawable.biggest,
+            "Antarctic blue whale",
+            "Narwhal",
+            "African Elephant",
+            "Rhino",
+            2
+        )
+        val question6 = Question(
+            1,
+            "What is the most dangerous liquid in the world?",
+            R.drawable.liquids,
+            "Ricin",
+            "Botulinum toxin",
+            "Batrachotoxin",
+            "Maitotoxin",
+            2
+        )
+        val question7 = Question(
+            1,
+            "What is the closest galaxy to the Milky Way?",
+            R.drawable.galaxies,
+            "Draco II",
+            "Canis Major Dwarf",
+            "Andromeda",
+            "Tucana III",
+            3
+        )
+        val question8 = Question(
+            1,
+            "What is the highest building in the world?",
+            R.drawable.scyscrapers,
+            "Merdeka 118",
+            "Lotte World Tower",
+            "Shanghai Tower",
+            "Burj Khalifa",
+            4
+        )
+        questionsList.add(question1)
+        questionsList.add(question2)
+        questionsList.add(question3)
+        questionsList.add(question4)
+        questionsList.add(question5)
+        questionsList.add(question6)
+        questionsList.add(question7)
+        questionsList.add(question8)
+
+        return questionsList
+    }
+}
--- app/src/main/java/com/csibtn/quizapp/MainActivity.kt
+++ app/src/main/java/com/csibtn/quizapp/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.csibtn.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.csibtn.quizapp.databinding.ActivityMainBinding
+
+
+lateinit var binder : ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        binder = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binder.root)
+
+        val startButton = binder.btnStart
+
+        startButton.setOnClickListener{
+            val name = binder.etName.text.toString()
+            if(name.isNotEmpty()){
+                val intent = Intent(this,QuizQuestionsActivity::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
